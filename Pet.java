cf112b370a994dd0a96a26c10cd0fa3d6e61fbe5
Grace <73140860+Young-Grace@users.noreply.github.com>
2020-11-17 09:08:13 -0500

    Add files via upload

--- Pet.java
+++ Pet.java
@@ -2,9 +2,49 @@ package practice;
 
 public class Pet {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String name;
+	private int age;
+	private String location;
+	private String type;
+	public Pet(String name, int age, String location, String type) {
+		this.name = name;
+		this.age = age;
+		this.location = location;
+		this.type = type;
 	}
+	public Pet() {
+		
+	}
+	public String getName() {
+		return name;
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	public int getAge() {
+		return age;
+	}
+	
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	public String getType() {
+		return type;
+	}
+	
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
 
 }
